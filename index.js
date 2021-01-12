82a9c5ad5bc1ebe44b1cc32dd8da7296c0b8747f
James Emanuel <jamesemanuel99@gmail.com>
2021-01-12 13:53:46 +0300

    wip: #4 Counting Downloads

--- index.js
+++ index.js
@@ -1,61 +1,83 @@
 const express = require('express');
-const cors = require('cors');
 const ytdl = require('ytdl-core');
+const mysql = require('mysql');
+const cors = require('cors');
 const app = express();
-const PORT = 4000;
+
+const PORT = 3000;
+
+const con = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'emanate_pakua'
+});
+
 
 app.use(cors());
 
 app.listen(PORT, () => {
-	console.log(`Server Works !!! At port ${PORT}`);
+    console.log(`Go Force, Run Force!!! At port ${PORT}`);
 });
 
 app.get('/downloadmp3', async (req, res, next) => {
-	try {
-		var url = req.query.url;
-		if(!ytdl.validateURL(url)) {
-			return res.sendStatus(400);
-		}
-		let title = 'audio';
+    try {
+        var url = req.query.url;
+        if (!ytdl.validateURL(url)) {
+            return res.sendStatus(400);
+        }
+        let title = 'audio';
 
-		await ytdl.getBasicInfo(url, {
-			format: 'mp4'
-		}, (err, info) => {
-			if (err) throw err;
-			title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
-		});
+        await ytdl.getBasicInfo(url, {
+            format: 'mp4'
+        }, (err, info) => {
+            if (err) throw err;
+            title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+        });
 
-		res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
-		ytdl(url, {
-			format: 'mp3',
-			filter: 'audioonly',
-		}).pipe(res);
+        res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
+        ytdl(url, {
+            format: 'mp3',
+            filter: 'audioonly',
+        }).pipe(res);
 
-	} catch (err) {
-		console.error(err);
-	}
+        con.getConnection(function (err, connection) {
+            connection.query('UPDATE downloads SET counts = counts + 1 ', function (err, rows) {
+                connection.release();
+            });
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 app.get('/downloadmp4', async (req, res, next) => {
-	try {
-		let url = req.query.url;
-		if(!ytdl.validateURL(url)) {
-			return res.sendStatus(400);
-		}
-		let title = 'video';
+    try {
+        let url = req.query.url;
+        if (!ytdl.validateURL(url)) {
+            return res.sendStatus(400);
+        }
+        let title = 'video';
 
-		await ytdl.getBasicInfo(url, {
-			format: 'mp4'
-		}, (err, info) => {
-			title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
-		});
+        await ytdl.getBasicInfo(url, {
+            format: 'mp4'
+        }, (err, info) => {
+            title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+        });
 
-		res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
-		ytdl(url, {
-			format: 'mp4',
-		}).pipe(res);
+        res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
+        ytdl(url, {
+            format: 'mp4',
+        }).pipe(res);
 
-	} catch (err) {
-		console.error(err);
-	}
+        con.getConnection(function (err, connection) {
+            connection.query('UPDATE downloads SET counts = counts + 1 ', function (err, rows) {
+                connection.release();
+            });
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
 });
